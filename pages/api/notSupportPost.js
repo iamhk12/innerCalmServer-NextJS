cc23445d23968bfa2948654dea78fc16ef33b5fa
Himanshu Kothari <hkothari247@gmail.com>
2023-09-04 22:48:18 +0530

    Update notSupportPost.js

--- pages/api/notSupportPost.js
+++ pages/api/notSupportPost.js
@@ -1,26 +1,23 @@
 import connectDB from '../../db'; // Import your database connection setup
 import Post from '../../models/post';
-import Cors from 'cors'; // Import the Cors middleware
 
 connectDB(); // Initialize your database connection
-
-const cors = Cors({
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include all HTTP methods
-  origin: '*', // Set the allowed origin to your desired value
-});
 
 const notSupportPost = async (req, res) => {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    
-  // Apply CORS middleware
-  await cors(req, res);
 
   const { id } = req.query; // Use req.query to get the id parameter
 
-  console.log("Supporting post with id:", id);
+  console.log("Not Supporting post with id:", id);
+
+  if (req.method === 'OPTIONS') {
+    // Respond to preflight request with a 200 OK status
+    res.status(200).end();
+    return;
+  }
 
   if (req.method === 'PUT') {
     try {
@@ -30,8 +27,9 @@ const notSupportPost = async (req, res) => {
         return res.status(404).json({ error: 'Post not found' });
       }
 
-      if (postToUpdate.supports > 0)
+      if (postToUpdate.supports > 0) {
         postToUpdate.supports -= 1;
+      }
 
       await postToUpdate.save();
 
