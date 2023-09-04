5c1d08ea44652abfa62a9a7a44a95db51131d2cb
Himanshu Kothari <hkothari247@gmail.com>
2023-09-04 22:35:15 +0530

    Update allPosts.js

--- pages/api/allPosts.js
+++ pages/api/allPosts.js
@@ -1,19 +1,21 @@
 import Post from '../../models/post';
 import connectDB from '../../db';
-import Cors from 'cors';
-
-// Configure CORS to allow requests from a specific origin
-const cors = Cors({
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include all HTTP methods
-  origin: '*', // Set the allowed origin to your Netlify app URL
-});
 
 connectDB();
 
 export default async function handler(req, res) {
-  // Enable CORS for this route
-  await cors(req, res);
-  console.log("endpoint /allPosts called");
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Check if it's a preflight request (OPTIONS) and handle it
+  if (req.method === 'OPTIONS') {
+    res.status(204).end(); // Respond with a 204 No Content for preflight requests
+    return;
+  }
+
+  console.log("Endpoint /allPosts called");
 
   if (req.method === 'GET') {
     try {
