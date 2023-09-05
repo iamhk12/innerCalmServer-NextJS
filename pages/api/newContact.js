5ac64ae6d39f5a486b525b1dc0b767d8735c56dc
Himanshu Kothari <hkothari247@gmail.com>
2023-09-05 19:33:52 +0530

    hk

--- pages/api/newContact.js
+++ pages/api/newContact.js
@@ -9,6 +9,12 @@ const newContact = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(204).end(); // Respond with a 204 No Content for preflight requests
+    return;
+  }
+
   
   if (req.method === 'POST') {
     const { name, email, phone, subject, message } = req.body;
